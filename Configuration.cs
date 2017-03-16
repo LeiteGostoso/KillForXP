49f7aa690e6f8f28a6a4ce939d05051b431dd6ec
LeiteGostoso <atherisgaming@aol.com>
2017-03-16 15:52:27 -0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.API;
+
+namespace KillForXP
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public uint Skull_XP, Arm_XP, Leg_XP, Body_XP, Foot_XP, Spine_XP, Front_XP, Back_XP;
+        public bool ConsoleMsgEnabled, PlayerMsgEnabled;
+        public float plrMsg_r, plrMsg_g, plrMsg_b;
+        public string consoleMsgColor;
+        public void LoadDefaults()
+        {
+            Skull_XP = 75;
+            Arm_XP = 30;
+            Leg_XP = 20;
+            Body_XP = 50;
+            Foot_XP = 10;
+            Spine_XP = 40;
+            Front_XP = 20;
+            Back_XP = 20;
+            ConsoleMsgEnabled = true;
+            PlayerMsgEnabled = true;
+            plrMsg_r = 255;
+            plrMsg_g = 0;
+            plrMsg_b = 0;
+            consoleMsgColor = "red";
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Events;
+using SDG.Unturned;
+using Rocket.API.Collections;
+using UnityEngine;
+
+namespace KillForXP
+{
+    public class Plugin : RocketPlugin<Configuration>
+    {
+        private DateTime lastCalled = DateTime.Now;
+
+        protected override void Load()
+        {
+            Rocket.Core.Logging.Logger.Log("XP For Kill loaded successfully.");
+        }
+
+        public void FixedUpdate()
+        {
+            UnturnedPlayerEvents ev = new UnturnedPlayerEvents();
+            ev.OnDeath += ev_OnDeath;
+        }
+        public static Plugin Instance;
+        void ev_OnDeath(UnturnedPlayer player, SDG.Unturned.EDeathCause cause, SDG.Unturned.ELimb limb, Steamworks.CSteamID murderer)
+        {
+            UnturnedPlayer killer = UnturnedPlayer.FromCSteamID(murderer);
+            if (limb == ELimb.SKULL)
+                GiveXP(Instance.Configuration.Instance.Skull_XP, killer, player, Translate("skull_name"));
+            if (limb == ELimb.LEFT_ARM || limb == ELimb.RIGHT_ARM)
+                GiveXP(Instance.Configuration.Instance.Arm_XP, killer, player, Translate("arm_name"));
+            if (limb == ELimb.LEFT_FOOT || limb == ELimb.RIGHT_FOOT)
+                GiveXP(Instance.Configuration.Instance.Foot_XP, killer, player, Translate("foot_name"));
+            if (limb == ELimb.RIGHT_FRONT || limb == ELimb.LEFT_FRONT)
+                GiveXP(Instance.Configuration.Instance.Front_XP, killer, player, Translate("front_name"));
+            if (limb == ELimb.SPINE)
+                GiveXP(Instance.Configuration.Instance.Spine_XP, killer, player, Translate("spine_name"));
+            if (limb == ELimb.LEFT_LEG || limb == ELimb.RIGHT_LEG)
+                GiveXP(Instance.Configuration.Instance.Leg_XP, killer, player, Translate("leg_name"));
+            if (limb == ELimb.LEFT_BACK || limb == ELimb.RIGHT_BACK)
+                GiveXP(Instance.Configuration.Instance.Back_XP, killer, player, Translate("back_name"));
+        }
+
+        void GiveXP(uint amount, UnturnedPlayer player, UnturnedPlayer killed, string limbName)
+        {
+            player.Experience += amount;
+            Color c = new Color();
+            c.r = Instance.Configuration.Instance.plrMsg_r;
+            c.g = Instance.Configuration.Instance.plrMsg_g;
+            c.b = Instance.Configuration.Instance.plrMsg_b;
+            UnturnedChat.Say(player, Translate("msg_playermsg", amount, killed, limbName), c);
+            ConsoleColor c2 = ConsoleColor.White;
+            try
+            {
+                c2 = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), Instance.Configuration.Instance.consoleMsgColor, true);
+                Rocket.Core.Logging.Logger.Log(Translate("msg_consolemsg"), c2);
+            }
+            catch
+            {
+                Rocket.Core.Logging.Logger.Log("WRONG COLOR SYNTAX IN XP FOR KILL PLUGIN");
+                Rocket.Core.Logging.Logger.Log(Translate("msg_consolemsg"), ConsoleColor.Red);
+            }
+        }
+
+        public new TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList(){
+                    {"msg_playermsg","You received {0} experience by killing {1} in the {2}."},
+                    {"msg_consolemsg","{0} received {1} experience by killing {2} in the {3}."},
+                    {"spine_name", "spine"},
+                    {"arm_name", "arm"},
+                    {"leg_name", "leg"},
+                    {"back_name", "back"},
+                    {"front_name", "front"},
+                    {"foot_name", "foot"},
+                    {"skull_name", "skull"}
+                };
+            }
+        }
+    }
+}
